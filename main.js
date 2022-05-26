42bb4c558d459feb819bbbd9a2b7ba39bc63e423
anitso <90929385+anitso@users.noreply.github.com>
2022-05-26 22:51:20 +0400

    edit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
   .then(json => console.log(json))
-fetch('https://jsonplaceholder.typicode.com/posts')
+  fetch('https://jsonplaceholder.typicode.com/posts')
   .then(response => response.json())
   .then(json => console.log(json))
